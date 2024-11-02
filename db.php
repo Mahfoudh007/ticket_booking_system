985862b7e08b4034a2c40d381b6085f1781507a3
mahfoudh <mahfoodh20015@gmail.com>
2024-11-02 15:02:16 +0300

    I added db.php for Conn with database and index.html for home page

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php 
+//Database connection 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "TicketBookingSystem";
+
+try {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+} catch (Exception $e) {
+    error_log($e->getMessage());
+    exit('Error connecting to the database');
+} 
+?>
